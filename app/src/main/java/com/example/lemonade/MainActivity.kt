a761cb3bd100c7074fa37e5a432a4c63ef4ef887
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-03-05 15:07:17 +0000

    feat: Implemented the top bar of the app.

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,13 +3,34 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +40,10 @@ class MainActivity : ComponentActivity() {
             LemonadeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    //modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LemonadeApp()
                 }
             }
         }
@@ -30,17 +51,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun LemonadeApp() {
+    TopBar(text = stringResource(R.string.app_name))
+}
+
+@Composable
+fun TopBar(
+    text: String,
+    modifier: Modifier = Modifier) {
+        Text(
+        text = text,
+            fontSize = 20.sp,
+        fontStyle = FontStyle.Normal,
+        fontWeight = FontWeight.ExtraBold,
+        textAlign = TextAlign.Center,
         modifier = modifier
+            .background(Color.Yellow)
+            .padding(top = 25.dp, bottom = 16.dp)
+            .height(40.dp)
+            .fillMaxWidth()
     )
 }
+
+//@Composable
+//fun LemonadeImageClickable(modifier: Modifier = Modifier) {
+//    var result by remember { mutableListOf(1) }
+//    val imageResource = when (result) {
+//        1 -> R.drawable.lemon_squeeze
+//        2 -> R.drawable.lemon_drink
+//        3. -> R.drawable.lemon_restart
+//        else -> R.drawable.lemon_tree
+//    }
+//    Column(
+//        modifier = modifier,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//        Image(
+//            painter = painterResource(id = imageResource),
+//            contentDescription = null,
+//            Modifier.clickable { result }
+//        )
+//        Spacer(modifier = Modifier.height(16.dp))
+//
+//    }
+//
+//}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
-        Greeting("Android")
+        LemonadeApp()
     }
 }
